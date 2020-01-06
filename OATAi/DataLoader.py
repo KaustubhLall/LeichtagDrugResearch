b68ada00f5265094d4dae6f8fe5f9326786b51be
kaustubh <klall@ucsd.edu>
2020-01-07 00:29:11 +0800

    Combined dataset is missing a header,

--- OATAi/DataLoader.py
+++ OATAi/DataLoader.py
@@ -29,7 +29,7 @@ class DataLoaderMetabolite:
         print('Loaded in data, null values found: ', end=' ')
         print(df[pd.isnull(df).any(axis=1)])
 
-        label_index = 0  # this is from source
+        label_index = 1  # this is from source
         print("Data shape: ", df.shape[0])
 
         X = np.array([np.array(df.iloc[x, :]) for x in range(df.shape[0])])
@@ -59,7 +59,7 @@ class DataLoaderMetabolite:
         print('Loaded in data, null values found: ', end=' ')
         print(df[pd.isnull(df).any(axis=1)])
 
-        label_index = 0  # this is from source
+        label_index = 1  # this is from source
         print("Data shape: ", df.shape[0])
 
         X = np.array([np.array(df.iloc[x, :]) for x in range(df.shape[0])])
@@ -75,21 +75,25 @@ class DataLoaderMetabolite:
 
     def load_oat1_3_p_combined(self):
         """
-        Loads the for large fold metabolites.
+        Loads the for all metabolites, including liver.
         :return: X, Y, Header (names of features)
         :rtype:
         """
-        source_df = pd.read_csv('./datasets/metabolites/OAT1OAT3Big.csv')
+
+        return
+        # todo the dataset is missing a header, get the header before proceeding.
+        source_df = pd.read_csv('./datasets/metabolites/OAT1OAT3OATP.csv')
         source_df['SLC'] = source_df['SLC'].astype('category').cat.codes
 
-        to_drop = [0, 2, 3, 4, ]
+        # todo update this
+        to_drop = [0, 1, 3, 4, ]
 
         df = source_df.drop(source_df.columns[to_drop], axis=1)
 
         print('Loaded in data, null values found: ', end=' ')
         print(df[pd.isnull(df).any(axis=1)])
 
-        label_index = 0  # this is from source
+        label_index = 2  # this is from source
         print("Data shape: ", df.shape[0])
 
         X = np.array([np.array(df.iloc[x, :]) for x in range(df.shape[0])])
