b7448da0153a1f8df72458ecb024b932007716c5
kaustubh <klall@ucsd.edu>
2020-01-07 00:26:14 +0800

    Data loader complete and tested.

--- OATAi/DataLoader.py
+++ OATAi/DataLoader.py
@@ -45,9 +45,39 @@ class DataLoaderMetabolite:
 
     def load_oat1_3_big(self):
         """
-        Loads the for small fold metabolites.
-        :return:
-        :rtype: X, Y, header (names of features)
+        Loads the for large fold metabolites.
+        :return: X, Y, Header (names of features)
+        :rtype:
+        """
+        source_df = pd.read_csv('./datasets/metabolites/OAT1OAT3Big.csv')
+        source_df['SLC'] = source_df['SLC'].astype('category').cat.codes
+
+        to_drop = [0, 2, 3, 4, ]
+
+        df = source_df.drop(source_df.columns[to_drop], axis=1)
+
+        print('Loaded in data, null values found: ', end=' ')
+        print(df[pd.isnull(df).any(axis=1)])
+
+        label_index = 0  # this is from source
+        print("Data shape: ", df.shape[0])
+
+        X = np.array([np.array(df.iloc[x, :]) for x in range(df.shape[0])])
+        Y = np.array(source_df.iloc[:, label_index])
+
+        header = np.array(df.columns)
+
+        if self.scale:
+            feature_scaler = StandardScaler()
+            X = feature_scaler.transform(X)
+
+        return X, Y, header
+
+    def load_oat1_3_p_combined(self):
+        """
+        Loads the for large fold metabolites.
+        :return: X, Y, Header (names of features)
+        :rtype:
         """
         source_df = pd.read_csv('./datasets/metabolites/OAT1OAT3Big.csv')
         source_df['SLC'] = source_df['SLC'].astype('category').cat.codes
@@ -76,5 +106,5 @@ class DataLoaderMetabolite:
 
 dat = DataLoaderMetabolite()
 
-x, y, h = dat.load_oat1_3_small()
+x, y, h = dat.load_oat1_3_big()
 # print(dat.load_oat1_3_big())
